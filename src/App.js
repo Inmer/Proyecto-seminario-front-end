a171052a826070ac8cebdbfada8fa7f775bdd31c
inmer <inmerbalcarcel@gmail.com>
2020-07-31 18:16:38 -0600

    pantalla login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Form, Input, Button, Checkbox, Row, Col, Card } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 function App() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Row justify="space-around" align="middle" style={{ height: "100vh" }}>
+      <Col xs={20}>
+        <Card
+          style={{
+            boxShadow:
+              "0 1px 2px -2px rgba(0, 0, 0, 0.16), 0 3px 6px 0 rgba(0, 0, 0, 0.12), 0 5px 12px 4px rgba(0, 0, 0, 0.09)",
+            textAlign: "center",
+            maxWidth: "380px",
+            margin: "auto",
+          }}
+          title="Taller valle del sol"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Por favor ingresa tu usuario" },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Usuario"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Por favor ingresa tu contraseña" },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Contraseña"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Recordarme</Checkbox>
+              </Form.Item>
+
+              <a className="login-form-forgot" href="">
+                Recuperar contraseña
+              </a>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                block
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Acceder
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
   );
 }
 
